77788ac0d6dd2cc6397d2dbf1743071efcb508a5
Sean Tilley <sean@deadsuperhero.com>
2012-08-15 01:36:54 -0500

    Initial commit, now uses Zurb Foundation for toolkit for responsive experiments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogster::Application.config.secret_token = '97a722929c5c404eb8a1ab503cbd384abb1b920bdbfdb708067f8faaf570256de6d842188ee972167bf281c516a30a0cb1564b278f6e4771d87bd68396e08887'
